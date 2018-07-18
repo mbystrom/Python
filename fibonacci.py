665ccfb819451119c905db31859c078d8502a41d
Noah Bystrom <noah.bystrom@calbt.com>
2018-07-18 14:42:06 -0700

    More  R E C U R S I O N
    This time fibonacci numbers
    Also timing function length - this ones p shit

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+import time
+
+def fib (n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+x = []
+for i in range(1,11):
+    x.append(fib(i))
+
+for i in x:
+    print(i)
+
+y = 500
+t = time.time()
+z = fib(y)
+runtime = time.time() - t
+print(f"{z} is the {y}th fibonacci number \nfuck me, that took {runtime} seconds to complete")
