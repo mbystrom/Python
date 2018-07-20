43656b5a481c07f12db51f0e4eb773d1c24e983b
Noah Bystrom <noah.bystrom@calbt.com>
2018-07-20 15:01:37 -0700

    I put a lot of comments in so I won't confuse myself when I go back
    Prettified code some
    But mostly comments
    I only put a few extra whitespace linebreaks in, but it went from like
    80 to like 120 lines

--- maze3.py
+++ maze3.py
@@ -1,21 +1,35 @@
 import sys
 import random as r
 
+# 1000 is the default recursion limit (raise it for larger mazes)
+# 1000 only barely supports the current 50x40 maze
+sys.setrecursionlimit(3000)
+
+# directions correspond to bits
 N = 1
 S = 2
 E = 4
 W = 8
 
-DX       = { E: 1, W: -1, N:  0, S: 0 }
-DY       = { E: 0, W:  0, N: -1, S: 1 }
+# see assignment of nextX and nextY, that's why these are important
+# they allow us to assign direction without a bunch of ifs
+DX = { E: 1, W: -1, N:  0, S: 0 }
+DY = { E: 0, W:  0, N: -1, S: 1 }
+
+# this allows us to open the next tile towards the current tile
+#  _______
+# |_|_|_|_  <- corridors would look like that otherwise
+# because corridors would only be open from one side
 opposite = { E: W, W:  E, N:  S, S: N }
 
-width = 50
-height = 40
-depth = 0
+# globals for width and height of the maze
+width = 115
+height = 70
+
 
 def generate_matrix (width, height):
-    
+    # a simple process that slaps height lists of length width into a list
+
     matrix = []
 
     for i in range(height):
@@ -31,7 +45,9 @@ def generate_matrix (width, height):
     return matrix
 
 
-def isOutOfBounds(x, y, grid):
+def isOutOfBounds(x, y):
+    # check to make sure a coordinate is inside the area of the maze
+
     if x < 0 or x >= width - 1:
         return True
     if y < 0 or y >= height - 1:
@@ -39,6 +55,9 @@ def isOutOfBounds(x, y, grid):
     return False
 
 def CarvePassagesFrom(currentX, currentY):
+    # this is where the magic happens
+    # recursively grows a maze from coordinates given in the first call
+
     directions = [N, S, E, W]
     r.shuffle(directions)
 
@@ -46,7 +65,7 @@ def CarvePassagesFrom(currentX, currentY):
         nextX = currentX + DX[direction]
         nextY = currentY + DY[direction]
 
-        if isOutOfBounds(nextX, nextY, grid):
+        if isOutOfBounds(nextX, nextY):
             continue
 
         if grid[nextY][nextX] == 0:
@@ -56,7 +75,8 @@ def CarvePassagesFrom(currentX, currentY):
 
 
 def PrintMaze(grid):
-    
+    # prints out our maze
+
     # first line
     print("  ", end="")
     for i in range(width - 1):
@@ -64,22 +84,34 @@ def PrintMaze(grid):
     print("")
 
     for y in range(height-1):
+
+        # prints the west border
         print(" |", end="")
         for x in range(width-1):
+
+            # print a space if the coordinate contains south (is open)
             if grid[y][x] & S != 0:
                 print(" ", end="")
             else:
                 print("_", end="")
             
+            # print either underscore or space if coordinate contains east
             if grid[y][x] & E != 0:
+
+                # print a space if combined this and next tile contains south
                 if (grid[y][x] | grid[y][x+1]) & S != 0:
                     print(" ", end="")
                 else:
                     print("_", end="")
             else:
-                print("|", end="")
+                print("|", end="")    # print wall if east bit ot in coordinate
         print("")
 
+# create a matrix usin width and height globals
 grid = generate_matrix(width, height)
+
+# start recursion at the very beginning of our maze
 CarvePassagesFrom(0, 0)
+
+# print out our finished maze
 PrintMaze(grid)
