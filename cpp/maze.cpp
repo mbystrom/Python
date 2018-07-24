6e034d3b4bc0b913587511608f7758c33df118d6
Noah Bystrom <noah.bystrom@calbt.com>
2018-07-24 16:41:44 -0700

    C++ Maze is almost there
    Yes I'm still doing mazes
    I have a problem

--- cpp/maze.cpp
+++ cpp/maze.cpp
@@ -10,48 +10,60 @@ struct Directions
     const short E = 4;
     const short W = 8;
     
-    const std::map<int, int> DX {
-        { N, 0 },
-        { S, 0 },
-        { E, 1 },
-        { W, -1 }
-    };
     
-    const std::map<int, int> DY {
-        { N, -1 },
-        { S, 1 },
-        { E, 0 },
-        { W, 0 }
-    };
-    
-    const std::map<int, int> Opposite {
-        { N, S },
-        { S, N },
-        { E, W },
-        { W, E }
-    };
 };
 
 const int height = 20;
 const int width = 20;
 
 bool isOut (int x, int y);
+void CarveMaze (int cx, int cy, int (*grid)[height][width]);
+void PrintMaze (int (*grid)[height][width]);
+
+int main()
+{
+    int map[height][width] = { 0 };
+    CarveMaze(0, 0, &map);
+    PrintMaze(&map);
+}
+
 
 void CarveMaze (int cx, int cy, int (*grid)[height][width])
 {
     const Directions directions;
     int _dirs[4] = { directions.N, directions.S, directions.E, directions.W };
-    std::random_shuffle(_dirs[0], _dirs[4]);
+    std::random_shuffle(_dirs[0], _dirs[4]); // some error in random_shuffle here?
+    
+    std::map<int, int> DX {
+        { directions.N, 0 },
+        { directions.S, 0 },
+        { directions.E, 1 },
+        { directions.W, -1 }
+    };
+    
+    std::map<int, int> DY {
+        { directions.N, -1 },
+        { directions.S, 1 },
+        { directions.E, 0 },
+        { directions.W, 0 }
+    };
+    
+    std::map<int, int> Opposite {
+        { directions.N, directions.S },
+        { directions.S, directions.N },
+        { directions.E, directions.W },
+        { directions.W, directions.E }
+    };
     
     for (uint i = 0; i < (sizeof(_dirs)/sizeof(_dirs[0])); i++) {
-        int nx = cx + directions.DX[_dirs[i]];
-        int ny = cy + directions.DY[_dirs[i]];
+        int nx = cx + DX[_dirs[i]];
+        int ny = cy + DY[_dirs[i]];
         
         if (isOut(nx,ny)) continue;
         
         if (*grid[ny][nx] == 0) {
-            *grid[cy][cx] |= _dirs[i]
-            *grid[ny][nx] |= directions.Opposite[_dirs[i]]
+            *grid[cy][cx] |= _dirs[i];
+            *grid[ny][nx] |= Opposite[_dirs[i]];
             CarveMaze(nx, ny, &*grid);
         }
     }
@@ -66,8 +78,35 @@ bool isOut (int x, int y)
     return false;
 }
 
-int main()
+
+void PrintMaze(int (*grid)[height][width])
 {
-    int map[height][width] = { 0 };
-    CarveMaze(0, 0, &map);
+    using namespace std;
+    const Directions directions;
+    
+    for (int x = 0; x < width; x++) {
+        cout << "__";
+    }
+    cout << endl;
+    for (int y = 0; y < height; y++) {
+        cout << "|";
+        for (int x = 0; x < width; x++) {
+            if ((*grid[y][x] & directions.S) != 0) {
+                cout << " ";
+            } else {
+                cout << "_";
+            }
+            
+            if ((*grid[y][x] & directions.E) != 0) {
+                if (((*grid[y][x] | *grid[y][x+1]) & directions.S) != 0) {
+                    cout << " ";
+                } else {
+                    cout << "_";
+                }
+            } else {
+                cout << "|";
+            }
+        }
+        cout << endl;
+    }
 }
