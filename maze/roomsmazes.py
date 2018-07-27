31127347ff19d97ea2ed8cc328b4b6f172106bff
Noah Bystrom <noah.bystrom@calbt.com>
2018-07-27 12:14:42 -0700

    Convert to ASCII matrix ('#' and '.' instead of numbers) before printing
    Some code cleanup
    Add numeric print to bottom of roomslogic for safekeeping
    Add to-do to top of roomsmazes.py
    ASCII conversion allows direct printing via matrix.print_matrix

--- maze/roomsmazes.py
+++ maze/roomsmazes.py
@@ -1,3 +1,11 @@
+'''
+To-do:
+1. Flood fill to get regions
+2. Make sure all regions are connected
+3. Add extra connectors
+4. Uncarve unused tiles
+'''
+
 import sys
 import matrix
 import random as r
@@ -20,7 +28,7 @@ grid = matrix.generate_matrix(width, height, 0)
 
 minRoomSize = 3
 maxRoomSize = 8
-roomAttemps = 10
+roomAttemps = 25
 tilesInRooms = []
 
 def isOut (x, y):
@@ -32,7 +40,6 @@ def isOut (x, y):
 def CarveMaze (currentX, currentY):
   directions = [N, S, E, W]
   r.shuffle(directions)
-
   for direction in directions:
     nextX = currentX + DX[direction]
     nextY = currentY + DY[direction]
@@ -42,17 +49,14 @@ def CarveMaze (currentX, currentY):
     if grid[nextY][nextX] == 0:
       grid[currentY][currentX] |= direction
       grid[nextY][nextX] |= opposite[direction]
-
       CarveMaze(nextX, nextY)
 
 
 def AddRoom (xPos, yPos, roomWidth, roomHeight):
   # check if room is drawable
-
   drawable = True
   for y in range(roomHeight):
     for x in range(roomWidth):
-
       realX = x + xPos
       realY = y + yPos
       
@@ -68,25 +72,15 @@ def AddRoom (xPos, yPos, roomWidth, roomHeight):
   if drawable:
     for y in range(roomHeight):
       for x in range(roomWidth):
-
         realX = x + xPos
         realY = y + yPos
 
-        if y > 0:
-          grid[realY][realX] |= N
-        
-        if y < (roomHeight - 1):
-          grid[realY][realX] |= S
-        
-        if x > 0:
-          grid[realY][realX] |= W
-        
-        if x < (roomWidth - 1):
-          grid[realY][realX] |= E
+        if y > 0: grid[realY][realX] |= N
+        if y < (roomHeight - 1): grid[realY][realX] |= S
+        if x > 0: grid[realY][realX] |= W    
+        if x < (roomWidth - 1): grid[realY][realX] |= E
 
         tilesInRooms.append({'x': realX, 'y': realY})
-        print(grid[realY][realX], end="")
-      print("")
 
 
 def PlaceRooms (attempts):
@@ -115,47 +109,37 @@ def GetEmptyTiles ():
   
   return empty
 
-def PrintMaze(grid):
-  # first line
-  print("#", end="")
-  for i in range(width):
-    print("##", end="")
-  print("")
 
-  for y in range(height):
+def CreateASCIIMaze (grid):
+    maze = []
+    firstLine = []
+    firstLine.append('#')
+    for i in range(width):
+        firstLine.append('#')
+        firstLine.append('#')
+    maze.append(firstLine)
 
-    # prints the west border
-    print("#", end="")
-
-    for x in range(width):
-      if grid[y][x] == 0:
-        print("#", end="")
-      else:
-        print(".", end="")
-
-      if grid[y][x] & E != 0:
-        print(".", end="")
-      else:
-        print("#", end="")
-    
-    print("")
-
-    print("#", end="")
-    for x in range(width):
-        
-      if grid[y][x] & S != 0:
-        print(".", end="")
-      else:
-        print("#", end="")
-      
-      if grid[y][x] == 15 or (grid[y][x] == 14 and {'x': x, 'y': y} in tilesInRooms) \
-        or (grid[y][x] == 6 and {'x': x, 'y': y} in tilesInRooms) \
-        or (grid[y][x] == 7 and {'x': x, 'y': y} in tilesInRooms):
-          print(".", end="")
-      else:
-        print("#", end="")
-    
-    print("")
+    for y in range(height):
+        line = ['#',]
+        for x in range(width):
+            if grid[y][x] == 0: line.append('#')
+            else: line.append('.')
+            if grid[y][x] & E == 0: line.append('#')
+            else: line.append('.')
+        maze.append(line)
+        line = ['#',]
+        for x in range(width):
+            if grid[y][x] & S == 0: line.append('#')
+            else: line.append('.')
+            
+            if grid[y][x] == 15 or (grid[y][x] == 14 and {'x': x, 'y': y} in tilesInRooms) \
+              or (grid[y][x] == 6 and {'x': x, 'y': y} in tilesInRooms) \
+              or (grid[y][x] == 7 and {'x': x, 'y': y} in tilesInRooms):
+                line.append('.')
+            else:
+              line.append('#')
+        maze.append(line)
+    return maze
 
 PlaceRooms(roomAttemps)
 
@@ -165,4 +149,7 @@ while len(emptyTiles) > 0:
   CarveMaze(target['x'], target['y'])
   emptyTiles = GetEmptyTiles()
 
-PrintMaze(grid)
+asciiMaze = CreateASCIIMaze(grid)
+
+print("printing ASCII-ified version!")
+matrix.print_matrix(asciiMaze)
